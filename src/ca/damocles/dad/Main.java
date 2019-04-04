47d6a1e605b6b333b753eb7b66c62ff0398d08d4
Aikanwha <aikanwha@gmail.com>
2019-04-04 19:37:37 -0400

    finished basic framework.

---
+++ src/ca/damocles/dad/Main.java
@@ -0,0 +1,17 @@
+package ca.damocles.dad;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
